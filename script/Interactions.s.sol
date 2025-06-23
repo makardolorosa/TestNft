79f3b02609941827f8375f7f15154e3197881ae2
Mak <antonenko.makar123@gmail.com>
2025-06-23 17:39:44 -0400

    actual nft deploy to the testnet Sepolia

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,10 +6,14 @@ import {BasicNft} from "../src/BasicNft.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
-     string public constant PUDGE =
+    string public constant PUDGE =
         "https://bafybeicnjid52q5f5zrq6bs57bjviulqpycnxxdurlcyqrtchiyir6xwba.ipfs.dweb.link?filename=PudgeNft.json";
+
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
         mintNftOnContract(mostRecentlyDeployed);
     }
 
